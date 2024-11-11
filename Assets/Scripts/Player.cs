b7e2220941db6e32b2aa24887376188d883208fb
luvexsky <ngryemo@gmail.com>
2024-11-11 13:35:21 -0500

    Set player boundaries using ViewportToWorldPoint.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,25 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+
 public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5f;
     Vector2 rawInput;
+
+    [SerializeField] float paddingLeft = 0.5f;
+    [SerializeField] float paddingRight = 0.5f;
+    [SerializeField] float paddingTop = 0.5f;
+    [SerializeField] float paddingBottom = 0.5f;
+
+    Vector2 minBounds;
+    Vector2 maxBounds;
+
+    void Start()
+    {
+        InitBounds();
+    }
+
     void Update()
     {
         Move();
     }
 
+    void InitBounds()
+    {
+        Camera mainCamera = Camera.main;
+        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
+        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
+
+        // Adjust bounds to account for padding
+        minBounds.x += paddingLeft;
+        minBounds.y += paddingBottom;
+        maxBounds.x -= paddingRight;
+        maxBounds.y -= paddingTop;
+    }
+
     void Move()
     {
         Vector3 delta = rawInput * moveSpeed * Time.deltaTime;
-        transform.position += delta;
+        Vector2 newPos = new Vector2();
+
+        // Clamp the new position to stay within adjusted bounds
+        newPos.x = Mathf.Clamp(transform.position.x + delta.x, minBounds.x, maxBounds.x);
+        newPos.y = Mathf.Clamp(transform.position.y + delta.y, minBounds.y, maxBounds.y);
+
+        transform.position = newPos;
     }
 
-
     void OnMove(InputValue value)
-{
-    rawInput = value.Get<Vector2>();
-    Debug.Log(rawInput);
-}
+    {
+        rawInput = value.Get<Vector2>();
+        Debug.Log(rawInput);
+    }
 }
